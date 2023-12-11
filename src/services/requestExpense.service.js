86907eb562bffddeafac8c5ebc4a4aca697291c4
brahian github <brahiangithub@gmail.com>
2023-12-11 16:31:26 -0500

    enrutando

--- src/services/requestExpense.service.js
+++ src/services/requestExpense.service.js
@@ -2,29 +2,29 @@ const requestExpenseSchema = require("../models/requestExpense.model")
 const statusSchema = require("../models/status.model")
 
 const listRequestExpense = async () => {
-   return await RequestExpenseSchema.find().populate({
+   return await requestExpenseSchema.find().populate({
       path: "status",
       model: statusSchema
    });
 }
 
 const showRequestExpense = async (id) => {
-   return await RequestExpenseSchema.findById(id).populate({
+   return await requestExpenseSchema.findById(id).populate({
       path: "status",
       model: statusSchema
    })
 }
 
 const saveRequestExpense = async (RequestExpensive) => {
-   let RequestExpensive = new RequestExpenseSchema(RequestExpensive);
-   return await RequestExpensive.save();
+   let requestExp = new requestExpenseSchema(RequestExpensive);
+   return await requestExp.save();
 }
 
 const updateRequestExpense = async (RequestExpensive,id) => {
    try {
-      const requests = await RequestExpenseSchema.findById(id);
+      const requests = await requestExpenseSchema.findById(id);
       if (requests) {
-         await RequestExpenseSchema.findByIdAndUpdate(id, RequestExpensive)
+         await requestExpenseSchema.findByIdAndUpdate(id, RequestExpensive)
          return "update";
       } else {
         return "No found";
